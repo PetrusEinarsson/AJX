3bb3b6e6803ffddb9b86c6bba29164b9c9c502b3
Hannes <hannes.marklund@umu.se>
2026-02-10 11:34:32 +0100

    Remove broken import

--- ajx/sensors/__init__.py
+++ ajx/sensors/__init__.py
@@ -1,5 +1,4 @@
 from ajx.sensors.rotary_encoder import *
 from ajx.sensors.absolute_rotation import *
 from ajx.sensors.distance_sensor import *
-from ajx.sensors.inclination_sensor import *
 from ajx.sensors.base import *
